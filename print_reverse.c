00789f7004d1a0a04ec9d6fd146604b06acceeec
Shem Murimi <murithi.murimi23@students.dkut.ac.ke>
2024-10-02 21:17:59 +0300

    Add simple function that prints in reverse

---
+++ print_reverse.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void print_reverse(char *s)
+{
+    size_t len = strlen(s);
+
+    char *t = s + len - 1;
+
+    while(t >= s)
+    {
+        printf("%c", *t);
+        t = t - 1;
+    }
+    puts("");
+}
+
+
+int main()
+{
+    print_reverse("usa");
+    return 0;
+}
+
